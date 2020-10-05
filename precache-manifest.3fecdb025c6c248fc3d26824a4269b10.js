576ce1335bea52135e7f1937d77b05894adc8fb3
Samantha Lau <samantha.lhe@gmail.com>
2020-10-05 16:03:20 +0800

    Updates

---
+++ precache-manifest.3fecdb025c6c248fc3d26824a4269b10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e71d826ec567ef6f7dc64c5722562580",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "87b03ef32a5fd0408072",
+    "url": "/robofriends/static/css/main.7165b722.chunk.css"
+  },
+  {
+    "revision": "47e1933332c207c8649e",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.58523ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b03ef32a5fd0408072",
+    "url": "/robofriends/static/js/main.f8f97cd5.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
